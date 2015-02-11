3f9417e848aa7a18cc7b13e8111dd36e55064481
MawiaHL <mawiteascc@gmail.com>
2015-02-11 17:58:12 +0530

    Create dsn.php

---
+++ dsn.php
@@ -0,0 +1,11 @@
+<?php
+   // $zoweb = mysql_connect("localhost","user","yourpass");
+   // if (!$zoweb) {
+   //   die('Could not connect to zoweb: ' . mysql_error());
+   // } 
+   // mysql_select_db("zotranslation",$zoweb); 
+   // $result = mysql_query("select * from zotab", $zoweb);
+
+$zoweb = 'sqlite:zotranslation.sqlite';
+
+?>
