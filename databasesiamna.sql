f62c510492f2b371ba180497f3ea05f733b30101
MawiaHL <mawiteascc@gmail.com>
2015-02-11 18:01:32 +0530

    Create databasesiamna.sql

---
+++ databasesiamna.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS "zotab";
+CREATE TABLE "zotab" ("id" INTEGER PRIMARY KEY  NOT NULL , "thumal" VARCHAR NOT NULL , "sawifiahna" TEXT NOT NULL );
+INSERT INTO "zotab" VALUES(1,'tlawmngaihna','<b>(n)</b>. altruism');
+INSERT INTO "zotab" VALUES(2,'bawkbawn','<b>(n)</b>. Brinjal.');
+INSERT INTO "zotab" VALUES(3,'hmangaihna','<b>n</b>. Love, affection');
+INSERT INTO "zotab" VALUES(4,'hmangaih','<b>(verb)</b>. feel deep affection or sexual love for (someone). <br>be in love with, be infatuated with, be smitten with, be besotted with, be passionate about;
+care very much for, feel deep affection for, hold very dear, adore, think the world of, be devoted to, dote on, cherish, worship, idolize, treasure, prize;
+informalbe mad/crazy/nuts/wild/potty about, have a pash on, carry a torch for.');
